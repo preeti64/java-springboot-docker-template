595b5b5c1cfb4ddaa379ba547c26e69c22352602
preeti64 <58847237+preeti64@users.noreply.github.com>
2024-08-20 10:22:59 +0200

    boiler template

---
+++ src/main/java/com/resttemplate/microservicestartertemplate/MicroserviceStarterTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.resttemplate.microservicestartertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStarterTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceStarterTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resttemplate/microservicestartertemplate/MicroserviceStarterTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resttemplate.microservicestartertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStarterTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
